91574d3a26c61423aed5c14a4d454c43d6fac77c
Chema Leboreiro <chema.leboreiro@ignite.mx>
2019-01-29 17:07:45 -0600

    Hack FT runner to be able to test staging

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -3,13 +3,18 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import WebDriverException
 import time
+import os
 
 MAX_WAIT = 10
+
 
 class NewVisitorTest(StaticLiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
+        staging_server = os.environ.get('STAGING_SERVER')
+        if staging_server:
+            self.live_server_url = 'http://' + staging_server
 
     def tearDown(self):
         self.browser.quit()
@@ -34,7 +39,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
         self.wait_for_row_in_list_table('1: testing')
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertAlmostEqual(
-        inputbox.location['x'] + inputbox.size['width'] / 2,
+            inputbox.location['x'] + inputbox.size['width'] / 2,
             512,
             delta=10
         )
@@ -51,7 +56,6 @@ class NewVisitorTest(StaticLiveServerTestCase):
                 if time.time() - start_time > MAX_WAIT:
                     raise e
                 time.sleep(0.5)
-
 
     def test_can_start_a_list_for_one_user(self):
         # user checks out the homepage of a to-do app
@@ -83,7 +87,8 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
         # The page updates again, and now shows both items on her list
         # user notices the url changed, after visiting the same
-        self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
+        self.wait_for_row_in_list_table(
+            '2: Use peacock feathers to make a fly')
         self.wait_for_row_in_list_table('1: Buy peacock feathers')
 
     def test_multiple_users_can_start_lists_at_different_urls(self):
@@ -99,8 +104,8 @@ class NewVisitorTest(StaticLiveServerTestCase):
         self.assertRegex(edith_list_url, '/lists/.+')
 
         # Now a new user, Francis, comes along to the site.
-        ## We use a new browser session to make sure that no information
-        ## of Edith's is coming through from cookies etc
+        # We use a new browser session to make sure that no information
+        # of Edith's is coming through from cookies etc
         self.browser.quit()
         self.browser = webdriver.Firefox()
 
